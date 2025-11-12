84858af9453e5aea50f625ba8aab9fb88b69723a
EduardoCasarrubias <L25550594@chihuahua2.tecnm.mx>
2025-11-12 00:36:38 -0600

    Add files via upload

---
+++ EVA3_8_SOBRECARGA/src/eva3_8_sobrecarga/EVA3_8_SOBRECARGA.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_8_sobrecarga;
+
+/**
+ *
+ * @author ecasa
+ */
+public class EVA3_8_SOBRECARGA {
+
+    
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println(100);
+        System.out.println(100.5);
+        System.out.println(true);
+        System.out.println('c');
+        System.out.println(args);
+    }
+    
+}
